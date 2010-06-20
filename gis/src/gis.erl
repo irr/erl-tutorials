ed6a7827003bbaf92889da5aba3dd03147e53ecd
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2010-06-20 12:35:06 -0300

    gen_fsm integration

--- gis/src/gis.erl
+++ gis/src/gis.erl
@@ -4,12 +4,9 @@
 
 -behaviour(gen_server).
 
--export([start_link/0, start_link/1, stop/0, test/2, init/1,
+-export([start_link/0, start_link/1, test/2, init/1,
          handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
-
-stop() ->
-    gen_server:cast(gis, stop).
 
 test(Id, X) ->
 	gen_server:call(Id, {test, X}).
